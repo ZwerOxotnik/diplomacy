b27c5cb56ed3a5726104fe55caa218371389a726
ZwerOxotnik <zweroxotnik@gmail.com>
2022-01-09 10:53:33 +0300

    Update to 2.12.2

--- settings.lua
+++ settings.lua
@@ -1,22 +1,9 @@
 data:extend({
-	{
-		type = "bool-setting",
-		name = "diplomacy_visible_all_teams",
-		setting_type = "runtime-global",
-		default_value = false
-	},
-	{
-		type = "bool-setting",
-		name = "diplomacy_entity_not_on_map",
-		setting_type = "startup",
-		default_value = false
-	},
-	{
-		type = "bool-setting",
-		name = "diplomacy_allow_mine_entity",
-		setting_type = "runtime-global",
-		default_value = false
-	},
+	{type = "bool-setting", name = "diplomacy_tech_tanks_count_switcher", setting_type = "startup", default_value = true},
+	{type = "bool-setting", name = "diplomacy_hp_rocket_silo_switcher", setting_type = "startup", default_value = true},
+	{type = "bool-setting", name = "diplomacy_entity_not_on_map", setting_type = "startup", default_value = false},
+	{type = "bool-setting", name = "diplomacy_visible_all_teams", setting_type = "runtime-global", default_value = false},
+	{type = "bool-setting", name = "diplomacy_allow_mine_entity", setting_type = "runtime-global", default_value = false},
 	{
 		type = "bool-setting",
 		name = "disable_diplomacy_on_entity_died",
@@ -24,24 +11,12 @@ data:extend({
 		default_value = false
 	},
 	{
-		type = "bool-setting",
-		name = "diplomacy_hp_rocket_silo_switcher",
-		setting_type = "startup",
-		default_value = true
-	},
-	{
 		type = "int-setting",
 		name = "diplomacy_hp_rocket_silo",
 		setting_type = "startup",
 		default_value = 50000,
 		minimum_value = 1,
-		maximum_value = 10000000000,
-	},
-	{
-		type = "bool-setting",
-		name = "diplomacy_tech_tanks_count_switcher",
-		setting_type = "startup",
-		default_value = true
+		maximum_value = 10000000000
 	},
 	{
 		type = "int-setting",
@@ -49,57 +24,50 @@ data:extend({
 		setting_type = "startup",
 		default_value = 1000,
 		minimum_value = 1,
-		maximum_value = 10000000000,
-	},
-	{
+		maximum_value = 10000000000
+	}, {
 		type = "bool-setting",
 		name = "diplomacy_tech_power_armor_2_count_switcher",
 		setting_type = "startup",
 		default_value = true
-	},
-	{
+	}, {
 		type = "int-setting",
 		name = "diplomacy_tech_power_armor_2_count",
 		setting_type = "startup",
 		default_value = 1000,
 		minimum_value = 1,
-		maximum_value = 10000000000,
-	},
-	{
+		maximum_value = 10000000000
+	}, {
 		type = "bool-setting",
 		name = "diplomacy_tech_uranium_ammo_count_switcher",
 		setting_type = "startup",
 		default_value = true
-	},
-	{
+	}, {
 		type = "int-setting",
 		name = "diplomacy_tech_uranium_ammo_count",
 		setting_type = "startup",
 		default_value = 1800,
 		minimum_value = 1,
-		maximum_value = 10000000000,
-	},
-	{
+		maximum_value = 10000000000
+	}, {
 		type = "int-setting",
 		name = "diplomacy_HP_forbidden_entity_on_killed",
 		setting_type = "runtime-global",
 		default_value = 300,
 		minimum_value = 1,
-		maximum_value = 100000000000,
-	},
-	{
+		maximum_value = 100000000000
+	}, {
 		type = "int-setting",
 		name = "diplomacy_HP_forbidden_entity_on_mined",
 		setting_type = "runtime-global",
 		default_value = 300,
 		minimum_value = 0,
-		maximum_value = 100000000000,
-	},
-	{
+		maximum_value = 100000000000
+	}, {
 		type = "string-setting",
 		name = "who_decides_diplomacy",
 		setting_type = "runtime-global",
 		default_value = "all_players",
 		allowed_values = {"all_players", "team_leader"}
-	},
+	}
 })
